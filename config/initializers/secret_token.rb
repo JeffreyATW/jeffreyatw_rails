70fc7a50b872c4522307c6d6933955d0c64dbbfc
Jeffrey Carl Faden <jeffrey@labzero.com>
2012-04-19 21:43:19 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeffreyatw::Application.config.secret_token = '5c516640c266a009e1c2b850b7c46c49f3995be8dc2228c8f09cf5f512c0bfd66d89548019fd38435dd75734cc02a8e246a30b2999dfe1299d1804210171c826'
