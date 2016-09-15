4773dce92c4ced249d63bd79460ee7adc704c38d
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-09-15 13:56:48 -0700

    Specify jeffreyatw-rails again

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, "jeffreyatw"
-set :repo_url,  "git@github.com:JeffreyATW/jeffreyatw_rails.git"
+set :repo_url,  "git@jeffreyatw-rails.github.com:JeffreyATW/jeffreyatw_rails.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
