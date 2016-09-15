ae93d515dc35a28ff706613801525287a551659d
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-09-15 12:14:03 -0700

    Update app name

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repository,  "git@jeffreyatw-rails.github.com:JeffreyATW/jeffreyatw_rails.g
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :deploy_to, "/home/jeffreyatw/webapps/rails"
+set :deploy_to, "/home/jeffreyatw/webapps/jeffreyatw"
 
 role :web, "web308.webfaction.com"                          # Your HTTP server, Apache/etc
 role :app, "web308.webfaction.com"                          # This may be the same as your `Web` server
@@ -16,9 +16,9 @@ set :user, "jeffreyatw"
 set :use_sudo, false
 default_run_options[:pty] = true
 
-default_environment['PATH'] = "/home/jeffreyatw/webapps/rails/gems/bin:$PATH"
-default_environment['GEM_PATH'] = "/home/jeffreyatw/webapps/rails/gems"
-default_environment['GEM_HOME'] = "/home/jeffreyatw/webapps/rails/gems"
+default_environment['PATH'] = "/home/jeffreyatw/webapps/jeffreyatw/gems/bin:$PATH"
+default_environment['GEM_PATH'] = "/home/jeffreyatw/webapps/jeffreyatw/gems"
+default_environment['GEM_HOME'] = "/home/jeffreyatw/webapps/jeffreyatw/gems"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -37,7 +37,13 @@ namespace :deploy do
   task :restart do
     run "#{deploy_to}/bin/restart"
   end
+
+  desc "Install npm modules"
+  task :npm_install do
+    run "cd #{current_path}; npm install"
+  end
 end
+after :deploy, 'deploy:npm_install'
 
 namespace :db do
 
