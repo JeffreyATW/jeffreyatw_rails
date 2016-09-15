d038246c7dc0c45021f0b731625030177f5549bc
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-09-15 13:53:39 -0700

    Fix cap 2 grammar

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, "jeffreyatw"
-set :repository,  "git@jeffreyatw-rails.github.com:JeffreyATW/jeffreyatw_rails.git"
+set :repo_url,  "git@github.com:JeffreyATW/jeffreyatw_rails.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
