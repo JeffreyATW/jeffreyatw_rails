f92702d5bf0f8bfbbd8ef951d2e85d7f7a86915d
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-03-06 14:45:11 -0800

    Remove more redirect routes

--- config/routes.rb
+++ config/routes.rb
@@ -50,69 +50,9 @@ Jeffreyatw::Application.routes.draw do
   #     resources :products
   #   end
 
-=begin
-  get "resume" => redirect("https://docs.google.com/document/pub?id=1cO25pchy9k4VPChGw4CLKbMa4UjMeTXcuc4o3VSYk4g&embedded=true")
-
-  get "noisegate" => "welcome#noisegate"
-  get "fwd-slides" => redirect("http://docs.google.com/present/view?id=dfjgtf36_41hmtj2td2")
-  get "fwd-setup" => redirect("https://noisebridge.net/wiki/Front-end_Web_Development#setup")
-
-  get "atom(.:format)" => redirect("/car/rss.xml")
-  get "characters.shtml" => redirect("/car/characters")
-  get "feed" => redirect("/blog/feed/")
-=end
-
   get "underwater" => "welcome#underwater"
 
   get 'freakmap' => 'freakmap#index'
-
-  legacy_dirs = ['28', 'Scripts', 'bob', 'bus', 'cgi', 'comics', 'css',
-               'davegetz', 'deadwinter', 'draggy', 'drawings', 'evidence',
-               'evildavis', 'facebook', 'ff', 'food', 'gailmuldrow', 'gordon',
-               'groza', 'holocaust', 'images', 'js', 'ld', 'mario', 'media',
-               'menorah', 'mid', 'midgarswamp', 'movies', 'mt', 'music', 'newsidebar',
-               'pants', 'photos', 'phpMyAdmin', 'professional', 'punks',
-               'r', 'rpgcomics', 'sa', 'saddam', 'sidebar', 'sillybaby',
-               'sm', 'sophie', 'store', 'tane', 'temp', 'v10', 'v11', 'v12',
-               'v3', 'v5', 'v6', 'v7', 'v8', 'v9', 'webcam']
-
-  legacy_dirs.each do |legacy_dir|
-    get "#{legacy_dir}(/:path(.:format))" => redirect {|params, req| "/static/#{legacy_dir}/#{params[:path]}.#{params[:format]}" }
-  end
-
-  legacy_files = ['Library.html', 'about.gif', 'about.shtml',
-                  'about_shadow.gif', 'archive.html', 'art.gif',
-                  'art_shadow.gif', 'atom.xml', 'bg.jpg', 'bio.html',
-                  'carbottom.html', 'carbottom_old.html', 'cartest.shtml',
-                  'cartop.html', 'cartop_old.html', 'characters.shtml', 'characters_old.shtml',
-                  'ck_yourmom.jpg', 'comic.html', 'comics.gif',
-                  'comics.shtml', 'comics_shadow.gif', 'contact.gif',
-                  'contact_shadow.gif', 'ddr_sims.gif', 'ddr_sims.shtml',
-                  'ddr_sims_shadow.gif', 'dropboard.swf', 'fanart.shtml',
-                  'finalproject.html', 'finalproject.swf', 'foaf.rdf',
-                  'footer.html', 'header.html', 'include.js', 'index.html',
-                  'index.php', 'index.rdf', 'index.xml', 'index_old.shtml',
-                  'index_v1.shtml', 'index_v2.shtml', 'index_v3.shtml',
-                  'index_v4.shtml', 'index_v5.shtml', 'index_v6.shtml',
-                  'ipod.html', 'kek.txt', 'legholestraws.txt', 'line.gif',
-                  'links.shtml', 'links.shtml', 'lj_logo.fla', 'lj_logo.swf',
-                  'lj_member.pdf', 'midi.shtml', 'misc.shtml', 'morlon.gif',
-                  'morlon.html', 'mythoughtsexactly.txt',
-                  'nav-commenters.gif', 'not_jeffrey.html', 'nutshell.txt',
-                  'pants.html', 'paul.html', 'paul.swf', 'polaroid.mov',
-                  'porterpamphlet.odg', 'porterpamphlet.pdf', 'saltykong.txt',
-                  'slug_lj.doc', 'slug_lj.pdf', 'style.css',
-                  'styles-site.css', 'styles.css', 'taka_death.html',
-                  'temp.txt', 'test.html', 'thesis.pdf', 'thisisafungame.txt',
-                  'too_many_eggs.jpg', 'unbendablegirder.txt', 'v5style.css',
-                  'v6_blogindex.html', 'v6_bright.html', 'v6_brtop.html',
-                  'v6_footer.html', 'v6_header.html', 'v6_top.html',
-                  'v6style.css', 'v7_bottom.txt', 'v7_top.txt', 'webcams.gif',
-                  'webcams_shadow.gif', 'whenmakefint.txt']
-
-  legacy_files.each do |legacy_file|
-    get "#{legacy_file}" => redirect("/static/#{legacy_file}")
-  end
 
   get "portfolio" => "portfolio#index", :as => :portfolio
   get "portfolio/feed(.:format)" => "portfolio#feed", :as => :portfolio_feed, :defaults => { :format => 'atom' }
